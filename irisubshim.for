2311163ee73cc18662e42f36156a2836d2a5720b
RenderHoward <David.Howard@Ultrapoise.com>
2024-11-15 11:12:03 -0500

    Corrected Fortran syntax

--- irisubshim.for
+++ irisubshim.for
@@ -1,10 +1,13 @@
        SUBROUTINE IRI_SUB_C(JF,JMAG,ALATI,ALONG,IYYYY,MMDD,DHOUR,
      & HEIBEG,HEIEND,HEISTP,OUTF,OARR) bind ( C, name="iri_sub_c" )
+       LOGICAL JF(50)
+
+       DIMENSION OUTF(20, 1000), OARR(100)
 
        call read_ig_rz
        call readapf107
 
-       call SUBROUTINE IRI_SUB( JF,JMAG,ALATI,ALONG,IYYYY,MMDD,DHOUR,
+       call IRI_SUB( JF,JMAG,ALATI,ALONG,IYYYY,MMDD,DHOUR,
      &   HEIBEG,HEIEND,HEISTP,OUTF,OARR )
 
        end
